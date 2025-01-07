36f6cc1047d014a5780cbe421eb887d4bd72666d
mesask <mesasokunlove@gmail.com>
2025-01-07 10:04:25 +0700

    Commit for Branch3

---
+++ StockManagement/Data/SMDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockManagement.Models.Domain;
+
+namespace StockManagement.Data;
+
+public class SMDbContext :  DbContext
+{
+    public SMDbContext(DbContextOptions<SMDbContext> options) : base( options)
+    {
+        
+    }
+
+    public DbSet<ItemType> ItemType { get; set; }
+    public DbSet<Unit> Unit { get; set; }
+}
--- StockManagement/Program.cs
+++ StockManagement/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using StockManagement.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<SMDbContext>(options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    }
+    );
 
 var app = builder.Build();
 
