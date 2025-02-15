07533a2cbcb4a91a28e2868d37221d8404d0bed7
mesask <mesasokunlove@gmail.com>
2025-02-15 10:08:04 +0700

    Commit for Unit Item

--- StockManagement/obj/Debug/net8.0/StockManagement.AssemblyInfo.cs
+++ StockManagement/obj/Debug/net8.0/StockManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1528eddcc14351fee76178360256e1b0c5a17f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a05f7b5f1a37e808d67d31e9bb46eeb90691fbfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
